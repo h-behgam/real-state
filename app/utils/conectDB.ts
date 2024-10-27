474f5e51bfc22cef01b7548edef94726e8b4064c
Hadi Behgam <h.behgam@gmail.com>
2024-10-27 14:01:00 +0330

    Update conectDB.ts

--- app/utils/conectDB.ts
+++ app/utils/conectDB.ts
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
 async function connectDB() {
-  if (mongoose.connections[0].readyState) return;
-  mongoose.set("strictQuery", false);
-  await mongoose.connect(process.env.MONGO_URI as string);
-  console.log("Connected to DB");
+  try {
+    if (mongoose.connections[0].readyState) return;
+
+    mongoose.set("strictQuery", false);
+    console.log("Connected");
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Connected to DB");
+    
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default connectDB;
