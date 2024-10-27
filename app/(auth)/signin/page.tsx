e01d7c22d84e9ebfbc99ce2e4e597a797b7fa371
Hadi Behgam <h.behgam@gmail.com>
2024-10-27 19:10:50 +0330

    Update page.tsx

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+
 import Signin from '../../components/UI/signin/signin'
 
-export default function SigninPage() {
+export default async function SigninPage() {
+  const session = await getServerSession(authOptions)
+  if (session) redirect('/')
   return (
     <Signin />
   )
