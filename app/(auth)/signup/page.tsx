840909071ba6f2a5da3b00cd706c03fb639f9b54
Hadi Behgam <h.behgam@gmail.com>
2024-10-27 19:10:47 +0330

    Update page.tsx

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
+
 import Signup from '../../components/UI/signup/signup'
 
-export default function SighUpPage() {
+export default async function SighUpPage() {
+  const session = await getServerSession(authOptions)
+  if (session) redirect('/')
+
   return (
     <Signup />
   )
