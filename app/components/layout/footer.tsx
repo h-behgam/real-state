551751e855e06ef59c1ede3c8ae0735195b502c2
Hadi Behgam <h.behgam@gmail.com>
2024-10-27 14:01:22 +0330

    Create footer.tsx

---
+++ app/components/layout/footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-blue-950 p-7 flex text-white justify-around">
+      <div className={""}>
+        <h3>سامانه خرید و اجاره ملک</h3>
+        <p>
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون
+          بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>تعرفه قانونی</li>
+          <li>دسترسی سریع</li>
+          <li>مشاورین خبره</li>
+          <li>قولنامه محضری</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
