2ba1cdde15e8c0c670ee6ec5c059528886912400
Hadi Behgam <h.behgam@gmail.com>
2024-10-27 14:01:24 +0330

    Create submit-form-button.tsx

---
+++ app/components/global/submit-form-button.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { cn } from '@/app/utils/utils'; 
+import { useFormStatus } from 'react-dom';
+
+export default function SubmitFormButton({
+  classname,
+  children,
+  onClick,
+}: {
+  // classname?: React.CSSProperties;
+  classname?: any;
+  onClick?: any;
+  children: React.ReactNode;
+}) {
+  const { pending } = useFormStatus();
+
+  return (
+    <div className='mb-3'>
+      <button
+        type='submit'
+        disabled={pending}
+        onClick={onClick ? onClick : null}
+        className={cn(
+          'rounded-lg bg-second_color px-7 py-2 text-white duration-300 hover:bg-hover_color disabled:bg-gray-400',
+          classname,
+        )}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
