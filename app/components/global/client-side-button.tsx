e8b88ddc3a741ccc513352b13aa653df7389eec1
Hadi Behgam <h.behgam@gmail.com>
2024-10-28 18:30:00 +0330

    Create client-side-button.tsx

---
+++ app/components/global/client-side-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { cn } from "@/app/utils/utils";
+import { ReactNode } from "react";
+
+interface InfoButtonProps {
+  children: ReactNode;
+  disabled: boolean;
+  classname?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  shadow?: boolean;
+  noCss?: boolean;
+}
+
+export default function ClientSideButton({ children, disabled, classname, onClick, shadow }: InfoButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={cn("rounded-full px-5 py-2 text-white duration-300", classname)}
+    >
+      {children}
+    </button>
+  );
+}
