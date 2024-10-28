433ada0dbc1f70810aad9e40151db71a82d63c54
Hadi Behgam <h.behgam@gmail.com>
2024-10-28 18:29:46 +0330

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function LayoutDashboard({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+import DashboardSidebar from "../components/layout/dashboard-sidebar";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
+  return <DashboardSidebar>{children}</DashboardSidebar>;
 }
