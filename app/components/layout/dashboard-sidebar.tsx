13bfdecf85fd98e84685fb6a051fceb8df162c8c
Hadi Behgam <h.behgam@gmail.com>
2024-10-30 17:43:33 +0330

    Update dashboard-sidebar.tsx

--- app/components/layout/dashboard-sidebar.tsx
+++ app/components/layout/dashboard-sidebar.tsx
@@ -4,17 +4,15 @@ import Link from "next/link";
 import { ReactNode } from "react";
 
 import { CgProfile } from "react-icons/cg";
-import ClientSideButton from "../global/client-side-button";
 
 import { FiLogOut } from "react-icons/fi";
-import { signOut } from "next-auth/react";
 import LogoutButton from "./logout-Button";
 
 export default async function DashboardSidebar({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
   return (
-    <div className="flex  gap-x-5 py-5">
-      <div className="shadow-lg shadow-gray-300 p-2 rounded-lg w-52">
+    <div className="flex  gap-x-6 py-5 px-1">
+      <div className="shadow-lg shadow-gray-300 p-2 rounded-lg w-52 font-normal">
         <CgProfile className="w-10 h-10 mx-auto my-3 text-blue-500 text-center" />
         <p className="my-8 text-center">{session?.user?.email}</p>
         <div className="flex flex-col">
@@ -27,7 +25,7 @@ export default async function DashboardSidebar({ children }: { children: ReactNo
           خروج
         </LogoutButton>
       </div>
-      <div>{children}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 }
