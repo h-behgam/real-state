69916b8a382475516e5683d98431341c7155c6a2
Hadi Behgam <h.behgam@gmail.com>
2024-10-31 23:27:03 +0330

    Update auth.ts

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,13 +1,64 @@
 import { hash, compare } from "bcryptjs";
 
+import connectDB from "@/app/utils/conectDB";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "../models/users";
+
+// hashed password
 async function hashPassword(password: string) {
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
 }
 
+//compare user input password and database password
 async function verifyPassword(password: string, hashedPassword: string) {
   const isValid = await compare(password, hashedPassword);
   return isValid;
 }
 
 export { hashPassword, verifyPassword };
+
+/**
+ * export authOptions for use in actions
+ */
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_KEY,
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+        },
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+      async authorize(credentials): Promise<any> {
+        // check database conect
+        try {
+          await connectDB();
+        } catch (error: any) {
+          throw new Error("مشکلی در ارتباط در بخش next auth وجود دارد");
+        }
+
+        // check user exist
+        const user = await User.findOne({ email: credentials?.email });
+        if (!user) {
+          throw new Error("ابتدا حساب کاربری ایجاد کنید");
+        }
+
+        // compare input password and user password
+        const passwordValid = await verifyPassword(credentials?.password!, user.password);
+        if (!passwordValid) {
+          throw new Error("رمز عبور اشتباه است");
+        }
+
+        return user;
+      },
+    }),
+  ],
+};
