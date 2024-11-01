6dc1fdc54f8d7cf8b55310ef560686aca0ce31d6
Hadi Behgam <h.behgam@gmail.com>
2024-11-01 14:31:06 +0330

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,6 +6,8 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import DashboardSidebar from "../components/layout/dashboard-sidebar";
 
+import "react-toastify/ReactToastify.min.css";
+
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
