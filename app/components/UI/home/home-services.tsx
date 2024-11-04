0a3b3edaeb094e5a2e629fb9f2de84610b707341
Hadi Behgam <h.behgam@gmail.com>
2024-11-04 17:49:17 +0330

    Create home-services.tsx

---
+++ app/components/UI/home/home-services.tsx
@@ -0,0 +1,16 @@
+import { services } from "@/constants/categories";
+
+import { FiCircle } from "react-icons/fi";
+
+export default function HomeServices() {
+  return (
+    <ul className="mx-auto my-5 text-center">
+      {services.map((i) => (
+        <li key={i} className="bg-blue-200 px-5  py-1 inline rounded-md mr-5">
+          <FiCircle className="inline ml-2" />
+          <span>{i}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
