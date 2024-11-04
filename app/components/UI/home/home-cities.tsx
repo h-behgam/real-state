6d7265e0d3d5186e0dd37fb561742885c2fda660
Hadi Behgam <h.behgam@gmail.com>
2024-11-04 17:49:20 +0330

    Create home-cities.tsx

---
+++ app/components/UI/home/home-cities.tsx
@@ -0,0 +1,18 @@
+import { cities } from "@/constants/categories";
+import { FaCity } from "react-icons/fa";
+
+export default function HomeCities() {
+  return (
+    <div>
+      <h2 className="font-semibold text-3xl text-center text-blue-800 my-10">شهر های پر بازدید</h2>
+      <ul className="flex justify-center flex-wrap">
+        {cities.map((i) => (
+          <li key={i} className="bg-blue-200 px-10 py-1 inline rounded-md mr-10 font-light">
+            <FaCity className="inline ml-2 text-blue-800" />
+            {i}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
