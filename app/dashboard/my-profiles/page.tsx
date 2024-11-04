4350850fa1e6bf8a79f1ea53fa9db840d8fec133
Hadi Behgam <h.behgam@gmail.com>
2024-11-04 17:49:13 +0330

    Update page.tsx

--- app/dashboard/my-profiles/page.tsx
+++ app/dashboard/my-profiles/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import ProfilesCards from "@/app/components/dashboard/my-profiles/profiles-cards";
+import User from "@/app/models/users";
+import { authOptions } from "@/app/utils/auth";
+import connectDB from "@/app/utils/conectDB";
+import { getServerSession } from "next-auth";
+import React from "react";
 
-export default function myProfilesPage() {
-  return (
-    <div>page</div>
-  )
+interface IProfile {
+  _id: number;
+  title: string;
+  description: string;
+  location: string;
+  phone: string;
+  realState: string;
+  price: number;
+  constructionDate: Date;
+  category: "villa" | "apartment" | "store" | "office";
+  amenities?: string[];
+  rules?: string[];
+}
+export default async function myProfilesPage() {
+  // try {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  const [user] = await User.aggregate([
+    { $match: { email: session?.user?.email } },
+    { $lookup: { from: "profiles", foreignField: "userId", localField: "_id", as: "profiles" } },
+  ]);
+
+  const profiles: IProfile[] = user.profiles;
+  // } catch (error) {
+  //   console.log('error in my profile and error is :', error);
+
+  // }
+
+  return profiles.map((item) => {
+    return <ProfilesCards card={JSON.parse(JSON.stringify(item))} key={item._id} />;
+  });
 }
