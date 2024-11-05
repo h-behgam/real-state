63d7f016d0bb145f9b5d6efe5e401321bebc962a
Hadi Behgam <h.behgam@gmail.com>
2024-11-05 19:41:39 +0330

    Update page.tsx

--- app/buy-residential/page.tsx
+++ app/buy-residential/page.tsx
@@ -1,15 +1,15 @@
-import Card, { ICard } from "@/app/components/dashboard/my-profiles/card";
+import { ICard } from "@/app/components/dashboard/my-profiles/card";
 
 import BuyResidentialMain from "../components/UI/buy-residential/main/buy-residential-main";
 import BuyResidentialSidebar from "../components/UI/buy-residential/sidebar/buy-residential-sidebar";
 
-type Params = { slug: string };
+// type Params = { slug: string };
 type SearchParams = { [key: string]: string | string[] | undefined };
 export default async function buyResidentialPage({
-  params,
+  // params,
   searchParams,
 }: {
-  params: Params;
+  // params: Params;
   searchParams: SearchParams;
 }) {
   const searchParam: string | string[] | undefined = Object.values(searchParams)[0];
