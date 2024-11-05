aca50e87e2da0484ecdf802a9d28419339850343
Hadi Behgam <h.behgam@gmail.com>
2024-11-05 13:47:27 +0330

    Update route.ts

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -5,17 +5,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
+    //get categort from request
+    const category = req.nextUrl.searchParams.get("category")
+    
     // conect to DB
     await connectDB();
 
-    const profiles = await Profile.find().select("-userId");
+    const profiles = await Profile.find(category? {category} : {}).select("-userId");
 
     // cjeck profile exist
     if (!profiles) {
       return NextResponse.json({ error: "پروفایلی یافت نشد!!!" }, { status: 404 });
     }
 
-    return NextResponse.json({ data: profiles });
+    return NextResponse.json(  profiles );
   } catch (error) {
     console.log("error", error);
     return NextResponse.json({ error: "مشکلی در سرور رخ داده است!" }, { status: 500 });
