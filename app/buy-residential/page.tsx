300517ee95e9f1b79898577741defe39c6ae46d8
Hadi Behgam <h.behgam@gmail.com>
2024-11-05 13:47:24 +0330

    Update page.tsx

--- app/buy-residential/page.tsx
+++ app/buy-residential/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import Card, { ICard } from "@/app/components/dashboard/my-profiles/card";
 
-export default function buyResidentialPage() {
+import BuyResidentialMain from "../components/UI/buy-residential/main/buy-residential-main";
+import BuyResidentialSidebar from "../components/UI/buy-residential/sidebar/buy-residential-sidebar";
+
+type Params = { slug: string };
+type SearchParams = { [key: string]: string | string[] | undefined };
+export default async function buyResidentialPage({
+  params,
+  searchParams,
+}: {
+  params: Params;
+  searchParams: SearchParams;
+}) {
+  const searchParam: string | string[] | undefined = Object.values(searchParams)[0];
+
+  const res = await fetch(
+    searchParam ? `http://localhost:3000/api/profile?category=${searchParam}` : "http://localhost:3000/api/profile",
+    { cache: "no-cache" }
+  );
+  const data: ICard[] = await res.json();
+
   return (
-    <div>/buy-residential:404</div>
-  )
+    <div className="flex gap-x-5 p-4">
+      <BuyResidentialSidebar />
+      <BuyResidentialMain data={data} />
+    </div>
+  );
 }
