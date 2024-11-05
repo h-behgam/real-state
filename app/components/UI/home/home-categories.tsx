0ad5bae53e838a13c4d20e303344c49347987247
Hadi Behgam <h.behgam@gmail.com>
2024-11-05 13:47:16 +0330

    Update home-categories.tsx

--- app/components/UI/home/home-categories.tsx
+++ app/components/UI/home/home-categories.tsx
@@ -1,17 +1,21 @@
+import { categories } from "@/constants/categories";
 import Image from "next/image";
 import Link from "next/link";
 
-interface ICategory {
-  title: string;
-  name: string;
-}
-export default function HomeCategories({ name, title }: ICategory) {
+export default function HomeCategories() {
   return (
-    <div className="w-60 p-3 shadow-[0_0px_14px_2px_rgba(96,125,250,0.3)] hover:-rotate-6 duration-300 rounded-md">
-      <Link href={"/"}>
-        <Image alt="" src={`/images/${name}.png`} width={240} height={144} />
-        <p className="text-center font-normal text-blue-800 mt-3">{title}</p>
-      </Link>
-    </div>
+    <>
+      {(Object.keys(categories) as Array<keyof typeof categories>).map((item) => (
+        <div
+          className="w-60 p-3 shadow-[0_0px_14px_2px_rgba(96,125,250,0.3)] hover:-rotate-6 duration-300 rounded-md"
+          key={item}
+        >
+          <Link href={{ pathname: "/buy-residential", query: { category: item } }}>
+            <Image alt="" src={`/images/${item}.png`} width={240} height={144} />
+            <p className="text-center font-normal text-blue-800 mt-3">{categories[item]}</p>
+          </Link>
+        </div>
+      ))}
+    </>
   );
 }
