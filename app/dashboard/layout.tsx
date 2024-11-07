cda1360a2ddbddab1bd9ce3eebadc3ebf4e5d505
Hadi Behgam <h.behgam@gmail.com>
2024-11-07 19:57:07 +0330

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,14 +2,34 @@ import { ReactNode } from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-import { authOptions } from '@/app/utils/auth'
+import { authOptions } from "@/app/utils/auth";
 
 import DashboardSidebar from "../components/layout/dashboard-sidebar";
 
 import "react-toastify/ReactToastify.min.css";
 
+import connectDB from "../utils/conectDB";
+import User from "../models/users";
+
+interface IUser {
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+}
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
-  return <DashboardSidebar>{children}</DashboardSidebar>;
+
+  //conect to DB
+  await connectDB();
+  const user = (await User.findOne({ email: session?.user?.email })) as unknown as IUser;
+
+  if (!user) return <h3>چنین کاربری وجود ندارد !!!</h3>;
+
+  return (
+    <DashboardSidebar email={session.user?.email} role={user.role}>
+      {children}
+    </DashboardSidebar>
+  );
 }
