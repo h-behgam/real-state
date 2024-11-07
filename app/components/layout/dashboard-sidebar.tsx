ea7a9976c639bf9bd69b7629a6947a7950079077
Hadi Behgam <h.behgam@gmail.com>
2024-11-07 19:57:25 +0330

    Update dashboard-sidebar.tsx

--- app/components/layout/dashboard-sidebar.tsx
+++ app/components/layout/dashboard-sidebar.tsx
@@ -1,5 +1,3 @@
-import { authOptions } from '@/app/utils/auth'
-import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { ReactNode } from "react";
 
@@ -8,17 +6,26 @@ import { CgProfile } from "react-icons/cg";
 import { FiLogOut } from "react-icons/fi";
 import LogoutButton from "./logout-Button";
 
-export default async function DashboardSidebar({ children }: { children: ReactNode }) {
-  const session = await getServerSession(authOptions);
+export default async function DashboardSidebar({
+  children,
+  email,
+  role,
+}: {
+  children: ReactNode;
+  email?: string | null;
+  role: string;
+}) {
   return (
     <div className="flex gap-x-6 py-5 px-1">
       <div className="shadow-lg shadow-gray-300 p-2 rounded-lg w-52 font-normal max-h-fit">
-        <CgProfile className="w-10 h-10 mx-auto my-3 text-blue-500 text-center" />
-        <p className="my-8 text-center">{session?.user?.email}</p>
+        <CgProfile className="w-10 h-10 mx-auto mt-3 text-blue-500 text-center" />
+        <p className="mb-8 text-center">{role === "ADMIN" ? "Admin" : null}</p>
+        <p className="my-8 text-center">{email}</p>
         <div className="flex flex-col">
           <Link href="/dashboard">حساب کاربری</Link>
           <Link href="/dashboard/my-profiles">آگهی های من</Link>
           <Link href="/dashboard/add">ثبت آگهی</Link>
+          {role === 'ADMIN' ? <Link href="/dashboard/pending">در انتظار تایید</Link> : null}
         </div>
         <LogoutButton>
           <FiLogOut />
