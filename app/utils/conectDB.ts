f7bd5301add98760f3f8a2d75db1cefb1e23c043
Hadi Behgam <h.behgam@gmail.com>
2024-11-07 19:57:01 +0330

    Update conectDB.ts

--- app/utils/conectDB.ts
+++ app/utils/conectDB.ts
@@ -5,9 +5,9 @@ async function connectDB() {
     if (mongoose.connections[0].readyState) return;
 
     mongoose.set("strictQuery", false);
-    console.log("Connected");
+    // console.log("Connected");
     await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("Connected to DB");
+    // console.log("Connected to DB");
     
   } catch (error) {
     console.log(error);
