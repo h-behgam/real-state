d3ac2515efdede45cdc6a6bea1a4a33eb6c399e5
Hadi Behgam <h.behgam@gmail.com>
2024-11-07 19:56:56 +0330

    Update icons.tsx

--- constants/icons.tsx
+++ constants/icons.tsx
@@ -2,7 +2,7 @@ import { RiHome3Line } from "react-icons/ri";
 import { MdApartment } from "react-icons/md";
 import { BiStore } from "react-icons/bi";
 import { GiOfficeChair } from "react-icons/gi";
-import { ReactElement, ReactNode } from "react";
+import { ReactNode } from "react";
 
 const icons: { [key: string]: ReactNode } = {
   villa: <RiHome3Line />,
