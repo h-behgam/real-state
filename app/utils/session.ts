031f6f51a2e9078f7404a3a6c57521c237a51148
Hadi Behgam <h.behgam@gmail.com>
2024-11-08 23:52:20 +0330

    Update session.ts

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -1,22 +1,54 @@
-import { getServerSession } from "next-auth";
+import { getServerSession, Session } from "next-auth";
 import { authOptions } from "./auth";
 import { redirect } from "next/navigation";
 import connectDB from "./conectDB";
 import User from "../models/users";
 
-export async function isSession() {
-  interface IUser {
-    email: string;
-    password: string;
-    role: string;
-    createdAt: Date;
-  }
+interface IUser {
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+}
+
+//check the session exist
+export async function checkSession() {
   const session = await getServerSession(authOptions);
-  if (!session) redirect("/signin");
+  return session ? session : undefined;
+}
 
-  //conect to DB
-  await connectDB();
-  const user = (await User.findOne({ email: session?.user?.email })) as unknown as IUser;
+// Checking session and returning user and session
+export async function isSession() {
+  //check session
+  const session = await checkSession();
 
-  return {user, session};
+  if (session) {
+    //conect to DB
+    await connectDB();
+    const user = await getUser(session);
+
+    return { user, session };
+  }
+  return undefined;
+}
+
+// // Checking session and returning user and session
+export async function getUser(session: Session) {
+  const user = (await User.findOne({ email: session.user?.email })) as IUser | undefined;
+  return user;
+}
+
+//check the role
+export async function checkRole() {
+  const result = await isSession();
+  const { user } = result ? result : { user: undefined };
+
+  switch (user?.role) {
+    case "ADMIN":
+      return "ADMIN";
+    case "USER":
+      return "USER";
+    default:
+      return "GUEST"; // مقدار پیش‌فرض
+  }
 }
