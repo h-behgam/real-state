e14d51f57d725da8ac3c369aac66da6217acbeba
Hadi Behgam <h.behgam@gmail.com>
2024-11-08 23:52:23 +0330

    Update page.tsx

--- app/dashboard/pending/page.tsx
+++ app/dashboard/pending/page.tsx
@@ -1,14 +1,16 @@
 import Profile from "@/app/models/profile";
 import connectDB from "@/app/utils/conectDB";
-import { isSession } from "@/app/utils/session";
+import { checkRole, isSession } from "@/app/utils/session";
 import { redirect } from "next/navigation";
+import { IProfile } from "../my-profiles/page";
+import ProfilePendingList from "@/app/components/dashboard/pending/profile-pending-list";
 
 export default async function PendingPage() {
-  const { user } = await isSession();
-  if (user.role !== "ADMIN") redirect("/dashboard");
+  const role = await checkRole();
+  if (role !== "ADMIN") redirect("/dashboard");
 
-  await connectDB()
-  const profile = await Profile.find({ published: false });
+  await connectDB();
+  const profiles = (await Profile.find({ published: false })) as unknown as IProfile[];
 
-  return <div>PendingPage</div>;
+  return profiles.map((item) => <ProfilePendingList profile={item} key={item._id} />);
 }
