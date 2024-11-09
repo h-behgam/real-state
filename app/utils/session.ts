1aa67172b0926c0e9942acb6973c931adc000102
Hadi Behgam <h.behgam@gmail.com>
2024-11-09 18:00:12 +0330

    Update session.ts

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -1,6 +1,5 @@
 import { getServerSession, Session } from "next-auth";
 import { authOptions } from "./auth";
-import { redirect } from "next/navigation";
 import connectDB from "./conectDB";
 import User from "../models/users";
 
