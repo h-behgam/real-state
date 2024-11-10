c061e65025fd5f9b4b16cf38ee5f89adca358244
Hadi Behgam <h.behgam@gmail.com>
2024-11-10 15:54:36 +0330

    Update page.tsx

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,13 +1,16 @@
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
-import { authOptions } from '@/app/utils/auth'
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/utils/auth";
+import type { Metadata } from "next";
 
-import Signin from '../../components/UI/signin/signin'
+export const metadata: Metadata = {
+  title: "صفحه ورود",
+  description: "ورود کاربران",
+};
+import Signin from "../../components/UI/signin/signin";
 
 export default async function SigninPage() {
-  const session = await getServerSession(authOptions)
-  if (session) redirect('/')
-  return (
-    <Signin />
-  )
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+  return <Signin />;
 }
