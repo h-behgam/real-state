24c273ee9d2137f827d277fa3a7ee8d69ff9356b
Hadi Behgam <h.behgam@gmail.com>
2024-11-10 16:15:40 +0330

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+import Profile from "./models/profile";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const staticRoutes = ["/", "/buy-residential", "/signup", "/signin"];
+
+  const profile = await Profile.find();
+  const profileId = profile.map((item) => ({
+    url: `http://localhost:3000/buy-residential/${item._id}`,
+    lastModified: new Date().toString(),
+  }));
+
+  const routes = staticRoutes.map((item) => ({
+    url: `http://localhost:3000${item}`,
+    lastModified: new Date().toString(),
+  }));
+
+  return [...routes, ...profileId];
+}
