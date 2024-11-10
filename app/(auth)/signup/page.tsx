2b45a8ca561cb4bdaf33b6e56276b8c8881fa784
Hadi Behgam <h.behgam@gmail.com>
2024-11-10 15:54:34 +0330

    Update page.tsx

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,12 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/utils/auth'
 import { redirect } from 'next/navigation'
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "صفحه ثبت نام",
+  description: "ثبت نام کاربران",
+};
 import Signup from '../../components/UI/signup/signup'
 
 export default async function SighUpPage() {
