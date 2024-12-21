1a02b33f6c9613c1f0f90f55e1f5e5dc57fa1b0b
Hadi Behgam <h.behgam@gmail.com>
2024-12-21 16:37:48 +0330

    Update home-cities.tsx

--- app/components/UI/home/home-cities.tsx
+++ app/components/UI/home/home-cities.tsx
@@ -5,10 +5,10 @@ export default function HomeCities() {
   return (
     <div>
       <h2 className="font-semibold text-3xl text-center text-blue-800 my-10">شهر های پر بازدید</h2>
-      <ul className="flex justify-center flex-wrap">
+      <ul className="flex justify-center flex-wrap gap-3">
         {cities.map((i) => (
-          <li key={i} className="bg-blue-200 px-10 py-1 inline rounded-md mr-10 font-light">
-            <FaCity className="inline ml-2 text-blue-800" />
+          <li key={i} className="bg-blue-200 px-10 py-1 flex rounded-md mr-10 font-light gap-x-2 items-center">
+            <FaCity className="text-blue-800" />
             {i}
           </li>
         ))}
